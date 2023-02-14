ea2877f4798fb338d9a12a5e53d8984d8870ba5b
TiEM <a52675268@gmail.com>
2023-02-14 11:15:36 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import { useState, useEffect } from "react";
+
+function Hello() {
+  function destryoFn() {
+    console.log("bye");
+  }
+  function effectFn() {
+    console.log("created");
+    return destryoFn;
+  }
+  useEffect(effectFn, []);
+
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
