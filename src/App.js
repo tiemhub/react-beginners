e4983c41334201aba5462ddc1ffa19ad937f7896
TiEM <a52675268@gmail.com>
2023-02-20 13:44:01 +0900

    ToDo 작성
    ToDo 코드 기록을 위한 커밋

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import Button from "./Button";
-import { useState, useEffect } from "react";
-
-function Hello() {
-  function destryoFn() {
-    console.log("bye");
-  }
-  function effectFn() {
-    console.log("created");
-    return destryoFn;
-  }
-  useEffect(effectFn, []);
-
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-
+  const [toDo, setToDo] = useState();
+  const [toDos, setToDos] = useState([]);
+  const onChangeToDo = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+    console.log(toDos);
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChangeToDo}
+          value={toDo}
+          type="text"
+          placeholder="Write To do"
+        />
+        <button>Add to Button</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((current, index) => (
+          <li key={index}>{current}</li>
+        ))}
+      </ul>
     </div>
   );
 }
