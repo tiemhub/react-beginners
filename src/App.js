8d29bdb3511ce3593f3f2a7cd9de939bc2bbfc1a
TiEM <a52675268@gmail.com>
2023-02-20 15:22:31 +0900

    영화 및 라우터 시작
    기록용

--- src/App.js
+++ src/App.js
@@ -1,37 +1,5 @@
-import { useState, useEffect } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState(1);
-  const onChange = (event) => setMoney(event.target.value);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  return (
-    <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : <strong>Loaded..</strong>}
-      <input
-        onChange={onChange}
-        value={money}
-        type="number"
-        placeholder="how many do you have money"
-      />
-      <ul>
-        {coins.map((coin) => (
-          <li key={coin.key}>
-            {coin.name}({coin.symbol}) : {money / coin.quotes.USD.price}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return null;
 }
 
 export default App;
